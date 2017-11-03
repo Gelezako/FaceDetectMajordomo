278ac090f885b0bd96fd5ec81039d884ec256356
Alex Sokolov <Gelezako@users.noreply.github.com>
2017-11-03 21:11:14 +0200

    Update checkface.bat

--- checkface.bat
+++ checkface.bat
@@ -1,4 +1,4 @@
-C:\_majordomo\apps\ffmpeg\bin\ffmpeg.exe -f dshow -i video="HP Webcam-101" -vframes 1 -f image2 C:\_majordomo\apps\facedetect\checkface.jpg
+rem теперь делается средствами MJD C:\_majordomo\apps\ffmpeg\bin\ffmpeg.exe -f dshow -i video="HP Webcam-101" -vframes 1 -f image2 C:\_majordomo\apps\facedetect\checkface.jpg
 c:
 cd C:\_majordomo\apps\facedetect\
 checkface_Alex.py
